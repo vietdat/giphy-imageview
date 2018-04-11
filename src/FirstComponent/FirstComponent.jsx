7669982f1264c3c1796ba25e364d3efaf2ad351e
vietdat1 <levietdat01091994@gmail.com>
2018-04-11 17:37:31 +0700

    sad

---
+++ src/FirstComponent/FirstComponent.jsx
@@ -0,0 +1,108 @@
+import React from 'react';
+
+class ImgageView extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="image-frame">
+          <div className="image">
+            <img src="https://media1.giphy.com/media/APpQYfZAYy7OIYmu5w/200_s.gif" alt="Mountain View"/>
+          </div>
+          <div>
+            <div className="attachent">
+              <img className="icon" src="../../image/attachment.svg" alt="attachment"/>
+            </div>
+            <div className="eye">
+              <img className="icon" src="../../image/eye.svg" alt="eye"/>
+              <span>1920</span>
+            </div>
+            <div className="comment">
+              <img className="icon" src="../../image/comment-white-oval-bubble.svg" alt="comment"/>
+              <span>129</span>
+            </div>
+            <div className="like">
+              <img className="icon" src="../../image/like.svg" alt="like" />
+              <span>20000</span>
+            </div>
+          </div>
+        </div>
+        <div className="image-frame">
+          <div className="image">
+            <img src="https://media1.giphy.com/media/APpQYfZAYy7OIYmu5w/200_s.gif" alt="Mountain View"/>
+          </div>
+          <div>
+            <div className="attachent">
+              <img className="icon" src="../../image/attachment.svg" alt="attachment"/>
+            </div>
+            <div className="eye">
+              <img className="icon" src="../../image/eye.svg" alt="eye"/>
+              <span>1920</span>
+            </div>
+            <div className="comment">
+              <img className="icon" src="../../image/comment-white-oval-bubble.svg" alt="comment"/>
+              <span>129</span>
+            </div>
+            <div className="like">
+              <img className="icon" src="../../image/like.svg" alt="like" />
+              <span>20000</span>
+            </div>
+          </div>
+        </div>
+        <div className="image-frame">
+          <div className="image">
+            <img src="https://media1.giphy.com/media/APpQYfZAYy7OIYmu5w/200_s.gif" alt="Mountain View"/>
+          </div>
+          <div>
+            <div className="attachent">
+              <img className="icon" src="../../image/attachment.svg" alt="attachment"/>
+            </div>
+            <div className="eye">
+              <img className="icon" src="../../image/eye.svg" alt="eye"/>
+              <span>1920</span>
+            </div>
+            <div className="comment">
+              <img className="icon" src="../../image/comment-white-oval-bubble.svg" alt="comment"/>
+              <span>129</span>
+            </div>
+            <div className="like">
+              <img className="icon" src="../../image/like.svg" alt="like" />
+              <span>20000</span>
+            </div>
+          </div>
+        </div>
+        <div className="image-frame">
+          <div className="image">
+            <img src="https://media1.giphy.com/media/APpQYfZAYy7OIYmu5w/200_s.gif" alt="Mountain View"/>
+          </div>
+          <div>
+            <div className="attachent">
+              <img className="icon" src="../../image/attachment.svg" alt="attachment"/>
+            </div>
+            <div className="eye">
+              <img className="icon" src="../../image/eye.svg" alt="eye"/>
+              <span>1920</span>
+            </div>
+            <div className="comment">
+              <img className="icon" src="../../image/comment-white-oval-bubble.svg" alt="comment"/>
+              <span>129</span>
+            </div>
+            <div className="like">
+              <img className="icon" src="../../image/like.svg" alt="like" />
+              <span>20000</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      
+    );
+  }
+}
+
+export default ImgageView;
